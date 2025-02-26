ab8968457d1bdb36d5f0f0cd3b50ea15401fd120
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-02-26 14:50:19 +0200

    modified:   App.tsx 	deleted:    components/Container.tsx 	deleted:    components/EditScreenInfo.tsx 	deleted:    components/ScreenContent.tsx 	new file:   nativewind-env.d.ts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
+    <SafeAreaView className='h-full bg-slate-600'>
+      <Text className='text-white text-center font-semibold text-[24px] mt-[24px]'>Dota.gg</Text>
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 }
