9074cce9aef88f9ff085825031facfbd908553ef
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-03-04 11:22:56 +0200

    Created filters by role and attribute

--- App.tsx
+++ App.tsx
@@ -2,12 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import './global.css';
 import {  SafeAreaView, Text, } from 'react-native';
 import List from 'components/List/List';
-import Filter from 'components/Filter/Filter';
 
 export default function App() {
   return (
-    <SafeAreaView className='flex-1 h-full bg-slate-600'>
-      <Filter/>
+    <SafeAreaView className='flex-1 h-full bg-slate-600 pt-[40px]'>
       <List/>
       <StatusBar style="auto" />
     </SafeAreaView>
--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,92 +1,92 @@
-import { useState, useEffect } from "react";
-import { Button, Pressable, Text, View } from "react-native";
-import DropDownPicker from "react-native-dropdown-picker";
-import axios from "axios";
+import React, { useCallback, useEffect, useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
+import { Text, View } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { FilterProps } from 'types/types';
 
-export default function Filter() {
-  const [typesOpen, setTypesOpen] = useState(false);
-  const [attributesOpen, setAttributesOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [valueAttribute, setValueAttributes] = useState(null);
-  const [types, setTypes] = useState<{ label: string; value: string }[]>([]);
+const Filter: React.FC<FilterProps> = ({ selectedRole, selectedPrimaryAttr, onRoleChange, onAttrChange, isChecked,  setIsChecked}) => {
+  const [roles, setRoles] = useState<{ label: string; value: string }[]>([]);
   const [attributes, setAttributes] = useState<{ label: string; value: string }[]>([]);
+  const [openRole, setOpenRole] = useState(false);
+  const [openAttr, setOpenAttr] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = useCallback(async () => {
       try {
-        const response = await axios.get("https://api.opendota.com/api/heroStats");
-  
+        const response = await axios.get('https://api.opendota.com/api/heroStats');
+
         const rolesSet = new Set<string>();
-        const attributesSet = new Set<string>();  
-  
         response.data.forEach((hero: any) => {
           hero.roles.forEach((role: string) => rolesSet.add(role));
-          if (hero.primary_attr) {
-            attributesSet.add(hero.primary_attr); 
-          }
         });
-  
-        const formattedRoles = Array.from(rolesSet)
-          .map((role) => ({
-            label: role,
-            value: role.toLowerCase(),
-          }))
-          .sort((a, b) => a.label.localeCompare(b.label));
-  
-        const formattedAttributes = Array.from(attributesSet)
-          .map((attribute) => ({
-            label: attribute.toUpperCase(),  
-            value: attribute.toLowerCase(),
-          }))
-          .sort((a, b) => a.label.localeCompare(b.label));
-  
-        setTypes(formattedRoles);
-        setAttributes(formattedAttributes);  
+
+        const formattedRoles = Array.from(rolesSet).map((role) => ({
+          label: role,
+          value: role.toLowerCase(),
+        })).sort((a, b) => a.label.localeCompare(b.label));
+
+        const attributesSet = new Set<string>();
+        response.data.forEach((hero: any) => {
+          attributesSet.add(hero.primary_attr);
+        });
+
+        const formattedAttributes = Array.from(attributesSet).map((attribute) => ({
+          label: attribute,
+          value: attribute.toLowerCase(),
+        })).sort((a, b) => a.label.localeCompare(b.label));
+
+        setRoles(formattedRoles);
+        setAttributes(formattedAttributes);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       }
-    };
-  
+    }, []);
+
+  useEffect(() => {
     fetchData();
-  }, []); 
+  }, [fetchData]);
 
   return (
-    <View className="p-4 flex">
-        <View className="flex-row gap-4">
-          <View className="flex-1 z-10">
-            <DropDownPicker
-                open={typesOpen}
-                value={value}
-                items={types} 
-                setOpen={setTypesOpen}
-                setValue={setValue} 
-                placeholder="Select a role"
-                style={{ backgroundColor: "#475569", borderColor: "white", borderWidth: 3}}
-                dropDownContainerStyle={{ backgroundColor: "#475569", borderColor: "white" }}
-                textStyle={{ color: "white", fontSize: 16 }}
-            />
-         </View>
-     
+    <View className="p-4 flex-col gap-[16px]">
+      <View className='flex-row gap-4'>
         <View className="flex-1 z-10">
-            <DropDownPicker
-              open={attributesOpen}
-              value={valueAttribute}
-              items={attributes} 
-              setOpen={setAttributesOpen}
-              setValue={setValueAttributes} 
-              placeholder="Select an attribute"
-              style={{ backgroundColor: "#475569", borderColor: "white", borderWidth: 3 }}
-              dropDownContainerStyle={{ backgroundColor: "#475569", borderColor: "white" }}
-              textStyle={{ color: "white", fontSize: 16 }}
-            />
-        </View>  
-       
-        </View>
-        <Pressable className = 'bg-blue-600 active:bg-blue-700 px-6 py-3 rounded-3xl shadow-md shadow-black/30 w-full mt-[10px]'>
-            <Text className="text-white font-semibold text-lg text-center">Save</Text>
-        </Pressable>
+        <DropDownPicker
+          open={openRole}
+          setOpen={setOpenRole}
+          value={selectedRole}
+          items={roles}
+          setValue={onRoleChange}
+          placeholder="Select a role"
+          style={{ backgroundColor: "#475569", borderColor: "#fff", width: '100%' }}
+          dropDownContainerStyle={{ backgroundColor: "#475569", borderColor: "white" }}
+          textStyle={{ color: "white", fontSize: 16 }}
+        />
+      </View>
+
+      <View className="flex-1 z-10">
+        <DropDownPicker
+          open={openAttr}
+          setOpen={setOpenAttr}
+          value={selectedPrimaryAttr}
+          items={attributes}
+          setValue={onAttrChange}
+          placeholder="Select an attribute"
+          style={{ backgroundColor: "#475569", borderColor: "#fff", width: '100%' }}
+          dropDownContainerStyle={{ backgroundColor: "#475569", borderColor: "white" }}
+          textStyle={{ color: "white", fontSize: 16 }}
+        />
+      </View>
+      </View>
       
+
+      <View className='flex-row items-center gap-[16px]'>
+        <Checkbox
+        value = {isChecked}
+        onValueChange={setIsChecked}/>
+        <Text className='text-white font-medium text-[16px]'>Sort by A-Z</Text>
+      </View>
     </View>
   );
-}
+};
 
+export default Filter;
